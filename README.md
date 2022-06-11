dda13811a21f30b7bf1447cc763dde806ffd599d
Hexa_Core <hexa_core@bluewin.ch>
2022-06-11 03:15:50 +0200

    rush00

---
+++ README.md
@@ -0,0 +1,29 @@
+# Rush00
+
+## How to use ?
+
+Add the following lines before the main function.
+
+```c
+#include "ft_putchar.c"
+#include "ft_rush0X.c"
+```
+
+## Norminette
+
+```shell
+norminette
+Rush00/main.c: OK!
+Rush00/ft_putchar.c: OK!
+Rush00/rush00.c: OK!
+Rush00/rush01.c: OK!
+Rush00/rush02.c: OK!
+Rush00/rush03.c: OK!
+Rush00/rush04.c: OK!
+```
+
+## How to clone ?
+
+```
+git clone git@github.com:H3x4Core/rush00.git
+```
---
+++ Rush00/rush02.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush02.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: H3x4Core <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/10 21:36:02 by m******e          #+#    #+#             */
+/*   Updated: 2022/06/11 03:03:58 by H3x4Core         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_line(int x, char left, char center, char right)
+{
+	int	i;
+
+	i = 0;
+	ft_putchar(left);
+	while (i++ < (x - 1))
+		ft_putchar(center);
+	if (x > 1)
+		ft_putchar(right);
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int	i;
+
+	i = 1;
+	if (x > 0 && y > 0)
+	{
+		ft_line(x, 'A', 'B', 'A');
+		while (i++ < (y - 1))
+			ft_line(x, 'B', ' ', 'B');
+		if (y > 1)
+			ft_line(x, 'C', 'B', 'C');
+	}
+}
